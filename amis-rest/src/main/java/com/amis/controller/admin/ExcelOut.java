7518dfb206fbaa72d92dcad3de32f631e6b63ab2
chenzexin <chenzexin0522@gmail.com>
2019-07-05 17:52:46 +0800

    新增下载

--- amis-rest/src/main/java/com/amis/controller/admin/ExcelOut.java
+++ amis-rest/src/main/java/com/amis/controller/admin/ExcelOut.java
@@ -511,6 +511,67 @@ public class ExcelOut {
         outputStream.close();
     }
 
+    @RequestMapping(value = "/downloadAimsOld")
+    public String downloadAims(HttpServletRequest request,
+                               HttpServletResponse response)throws UnsupportedEncodingException {
+        // 获取指定目录下的第一个文件     D://AMISJAVA//picture_apk//apk//OldBadminton.apk
+        File scFileDir = new File(" D://AMISJAVA//picture_apk//apk//OldBadminton.apk");
+        String fileName = scFileDir.getName(); //下载的文件名
+
+        // 如果文件名不为空，则进行下载
+        if (fileName != null) {
+            //设置文件路径
+            String realPath = "D://AMISJAVA//picture_apk//apk";
+            File file = new File(realPath, fileName);
+
+            // 如果文件名存在，则进行下载
+            if (file.exists()) {
+
+                // 配置文件下载
+                response.setHeader("content-type", "application/octet-stream");
+                response.setContentType("application/octet-stream");
+                // 下载文件能正常显示中文
+                response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+                // 实现文件下载
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    System.out.println("Download the song successfully!");
+                }
+                catch (Exception e) {
+                    System.out.println("Download the song failed!");
+                }
+                finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
 
     @RequestMapping(value = "/downloadAimsAPK")
     public String downloadAimsAPK(HttpServletRequest request,
