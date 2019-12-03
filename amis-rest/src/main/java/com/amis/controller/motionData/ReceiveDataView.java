81dd47efe6e7bd5be669262c989875517bbefaa1
chenzexin <chenzexin0522@gmail.com>
2019-12-03 11:05:56 +0800

    整理后健身房项目

--- amis-rest/src/main/java/com/amis/controller/motionData/ReceiveDataView.java
+++ amis-rest/src/main/java/com/amis/controller/motionData/ReceiveDataView.java
@@ -1,20 +1,7 @@
 package com.amis.controller.motionData;
 
-import com.amis.entity.dto.GradeDTO;
-import com.amis.entity.dto.ReslutMontionData;
-import com.amis.service.RestAdminService;
-import com.amis.service.impl.ReceiveDataServiceImpl;
-import org.apache.commons.collections.ArrayStack;
-import org.apache.commons.collections.FastArrayList;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 /**
  * @ClassName ReceiveDataView
@@ -26,10 +13,6 @@ import java.util.Random;
 @Controller
 @RequestMapping(value = "montion")
 public class ReceiveDataView {
-
-    @Autowired
-    private RestAdminService restAdminService;
-
     /**
      * @Author chenzexin
      * @Date 2019/5/31 10:09
